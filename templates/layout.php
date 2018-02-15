6f8f650f8728af8f0710e9f9ff067c567d0b731a
Lennore90 <lennore_90@mail.ru>
2018-02-16 00:39:40 +0300

    Задание №9, активный класс у проекта

--- templates/layout.php
+++ templates/layout.php
@@ -43,7 +43,8 @@
                 <nav class="main-navigation">
                     <ul class="main-navigation__list">
                         <? foreach ($project_list as $category): ?>
-                            <li class="main-navigation__list-item <?=$category == $project_list [0] ? 'main-navigation__list-item--active' : ''?>">
+                        	<? $index = $_GET['id']; ?>
+                            <li class="main-navigation__list-item <?=$category == $project_list[$index] ? 'main-navigation__list-item--active' : ''?>">
                                 <a class="main-navigation__list-item-link" href="/?id=<?=array_search($category, $project_list)?>#<?=$category?>"><?=$category?></a>
                                 <span class="main-navigation__list-item-count"><?=task_count($tasks, $category, $show_complete_tasks)?></span>
                             </li>
